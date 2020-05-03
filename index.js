457dbb0ced0f2cadd631df957f934bf980f4051e
Mykhailo418 <smmichael418@gmail.com>
2020-05-03 12:24:11 +0300

    SOLID: Dependency Inversion Principle

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // -- SOLID:
 //require('./solid/single_responsibility');
 //require('./solid/open_close');
-require('./solid/liskov_substitution');
+//require('./solid/liskov_substitution');
+//require('./solid/interface_segregation');
+require('./solid/dependency_inversion');

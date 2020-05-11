86c9e4ff4baf4fa29816b20ef948199ffc3a103d
Mykhailo418 <smmichael418@gmail.com>
2020-05-11 16:58:34 +0300

    Bridge

---
+++ bridge/bridge.js
@@ -0,0 +1,46 @@
+// Bridge: это структурный паттерн проектирования, который разделяет один или несколько классов на две отдельные иерархии — абстракцию и реализацию, позволяя изменять их независимо друг от друга.
+// Used to connect of few hierarchy classes
+
+class VectorRenderer {
+  renderCircle(radius) {
+    console.log(`Drawing a circle of radius ${radius}`);
+  }
+}
+
+class RasterRenderer {
+  renderCircle(radius) {
+    console.log(`Drawing pixels for circle of radius ${radius}`);
+  }
+}
+
+class Shape {
+  constructor(renderer) {
+    this.renderer = renderer;
+  }
+}
+
+class Circle extends Shape {
+  constructor(renderer, radius) {
+    super(renderer);
+    this.radius = radius;
+  }
+
+  draw() {
+    this.renderer.renderCircle(this.radius);
+  }
+
+  resize(factor) {
+    this.radius *= factor;
+  }
+}
+
+// imagine Square, Triangle
+// different ways of rendering: vector, raster
+// we don't want a cartesian product of these
+
+let raster = new RasterRenderer();
+let vector = new VectorRenderer();
+let circle = new Circle(vector, 5);
+circle.draw();
+circle.resize(2);
+circle.draw();
